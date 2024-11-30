1532272d2157fdfc3d2fdcc463699dfe1762ad76
doai123 <tongvandoai11111@gmail.com>
2024-11-30 14:22:10 +0700

    restart-spring

--- app/src/main/java/appsuckhoe/nghiemtuc/controller/KhachHangController.java
+++ app/src/main/java/appsuckhoe/nghiemtuc/controller/KhachHangController.java
@@ -1,8 +1,10 @@
     package appsuckhoe.nghiemtuc.controller;
 
     import org.springframework.stereotype.Controller;
+    import org.springframework.ui.Model;
     import org.springframework.web.bind.annotation.GetMapping;
-
+    import org.springframework.web.bind.annotation.PostMapping;
+    import org.springframework.web.bind.annotation.RequestParam;
 
     @Controller
     public class KhachHangController {
@@ -18,4 +20,17 @@
         public String home(){
             return "index";
     }
+    @PostMapping("/req/login")
+        public String login(@RequestParam("username") String username,
+                            @RequestParam("password") String password,
+                            Model model) {
+            if (username.isEmpty() || password.isEmpty()) {
+                model.addAttribute("error", "Username and password are required");
+                return "login";  // Trả lại trang login nếu thông tin không hợp lệ
+            }
+            // Tiến hành xác thực người dùng (sử dụng Spring Security hoặc phương pháp tùy chỉnh)
+            // Nếu đăng nhập thành công, chuyển hướng đến trang chính
+            return "redirect:/home";
+        }
+
     }
