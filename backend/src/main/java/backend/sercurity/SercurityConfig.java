57d831d33679dbb0f797898a14397df2635a7a54
doai123 <tongvandoai11111@gmail.com>
2024-12-05 13:50:13 +0700

    restart-spring

--- backend/src/main/java/backend/sercurity/SercurityConfig.java
+++ backend/src/main/java/backend/sercurity/SercurityConfig.java
@@ -5,9 +5,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -28,12 +30,20 @@ public class SercurityConfig {
     public UserDetailsService userDetailsService() {
         return khachHangService;
     }
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        System.out.println("Authenticate........!");
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .authenticationProvider(authenticationProvider())
+                .build();
+    }
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(khachHangService);
         provider.setPasswordEncoder(passwordEncoder());
+        System.out.println("AuthenticationProvider initialized!");
         return provider;
     }
 
